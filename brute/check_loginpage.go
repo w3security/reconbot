31a29c4d42acaedb988f390cb5b874ebdd250390
Common Vulnerabilities and Exploits Database <84289402+cvedb@users.noreply.github.com>
2023-02-12 20:50:19 -0800

    Create check_loginpage.go
    Signed-off-by: Common Vulnerabilities and Exploits Database <84289402+cvedb@users.noreply.github.com>

---
+++ brute/check_loginpage.go
@@ -0,0 +1,37 @@
+package brute
+
+import (
+	"github.com/w3security/reconbot/lib/util"
+	"net/url"
+	"regexp"
+	"strings"
+)
+
+func CheckLoginPage(inputurl string) bool {
+	if req, err := util.HttpRequset(inputurl, "GET", "", true, nil); err == nil {
+		cssurl := regexp.MustCompile(`<link[^>]*href=['"](.*?)['"]`).FindAllStringSubmatch(req.Body, -1)
+		for _, v := range cssurl {
+			if strings.Contains(v[1], ".css") {
+				u, err := url.Parse(strings.TrimSpace(inputurl))
+				if err != nil {
+					return false
+				}
+				href, err := url.Parse(strings.TrimSpace(v[1]))
+				if err != nil {
+					return false
+				}
+				if err != nil {
+					return false
+				}
+				hrefurl := u.ResolveReference(href)
+				if reqcss, err := util.HttpRequset(hrefurl.String(), "GET", "", true, nil); err == nil {
+					if util.StrContains(reqcss.Body, "login") {
+						return true
+					}
+				}
+			}
+		}
+		return false
+	}
+	return false
+}
