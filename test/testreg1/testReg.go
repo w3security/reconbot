9dcb084ae0011e300849cbe851c00f4d1a4e0e92
Common Vulnerabilities and Exploits Database <84289402+cvedb@users.noreply.github.com>
2023-02-12 20:16:17 -0800

    Create testReg.go
    Signed-off-by: Common Vulnerabilities and Exploits Database <84289402+cvedb@users.noreply.github.com>

---
+++ test/testreg1/testReg.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"github.com/w3security/reconbot/lib/util"
+)
+
+// 测试正则表达式是否正确
+func main() {
+	// 中途控制关闭当前目标所有fuzz
+	_, stop := context.WithCancel(util.Ctx_global)
+	stop()
+	stop()
+	stop()
+	stop()
+
+}
