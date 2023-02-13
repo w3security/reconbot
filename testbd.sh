ce93a337db2199b09a8504782908cf20d30a6373
Common Vulnerabilities and Exploits Database <84289402+cvedb@users.noreply.github.com>
2023-02-12 20:56:29 -0800

    Create testbd.sh
    Signed-off-by: Common Vulnerabilities and Exploits Database <84289402+cvedb@users.noreply.github.com>

---
+++ testbd.sh
@@ -0,0 +1,7 @@
+rm -rf vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/http/request.go
+rm -rf vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/interactsh/interactsh.go
+wget -c -O vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/http/request.go https://github.com/w3security/reconbot/raw/main/vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/http/request.go
+wget -c -O vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/interactsh/interactsh.go https://github.com/w3security/reconbot/raw/main/vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/interactsh/interactsh.go
+go build
+
+find . -name ".DS_Store" -delete
