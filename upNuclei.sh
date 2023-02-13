fa4326410da0cbe0e06a27edb6344ac706bbf1b4
Common Vulnerabilities and Exploits Database <84289402+cvedb@users.noreply.github.com>
2023-02-12 18:06:50 -0800

    Create upNuclei.sh
    Signed-off-by: Common Vulnerabilities and Exploits Database <84289402+cvedb@users.noreply.github.com>

---
+++ upNuclei.sh
@@ -0,0 +1,10 @@
+cd $HOME/MyWork/nuclei-templates
+git pull
+cd $HOME/MyWork/reconbot/
+ls ../nuclei-templates|xargs -I % cp -rf ../nuclei-templates/% config/nuclei-templates/
+git add config/nuclei-templates pocs_yml/ymlFiles vendor
+git checkout vendor/github.com/projectdiscovery/nuclei/v2
+git status
+find . -name ".DS_Store" -delete
+rm -rf logs/*
+rm -rf .DbCache
